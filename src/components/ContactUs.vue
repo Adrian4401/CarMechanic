7563984269c61f679566b0aceda29e742ab672d4
Adrian4401 <adrianzakrzewski4401@gmail.com>
2024-08-10 15:29:09 +0200

    Contact component finished.

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,23 +1,25 @@
 <script setup lang="ts">
     import InfoItem from '@/components/InfoItem.vue'
     import PhoneIcon from './icons/IconPhone.vue'
+    import EmailIcon from './icons/IconEmail.vue'
+    import PointIcon from './icons/IconPoint.vue'
 </script>
 
 <template>
     <InfoItem>
         <template #heading>Kontakt</template>
         <div>
-            <div class="flex items-center lg:text-2xl mt-6">
-                <PhoneIcon class="w-12 h-12 mr-8"/>
-                <a href="tel: 666666666" class="flex">666666666</a>
+            <div class="contact-element">
+                <PhoneIcon class="contact-element-icon"/>
+                <a href="tel:666666666">666666666</a>
             </div>
-            <div class="flex items-center lg:text-2xl mt-6">
-                <PhoneIcon class="w-12 h-12 mr-8"/>
-                <a href="tel: 666666666" class="flex">666666666</a>
+            <div class="contact-element">
+                <EmailIcon class="contact-element-icon"/>
+                <a href="mailto:pogromcamasla@gmail.com">pogromcamasla@gmail.com</a>
             </div>
-            <div class="flex items-center lg:text-2xl mt-6">
-                <PhoneIcon class="w-12 h-12 mr-8"/>
-                <a href="tel: 666666666" class="flex">666666666</a>
+            <div class="contact-element">
+                <PointIcon class="contact-element-icon"/>
+                <p class="pl-1">Łączna 3/4, Lipinki Łóżyckie</p>
             </div>
         </div>
     </InfoItem>
